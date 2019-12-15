3e9f792e4565a52f763715db4754ea1812d84b13
Paulo Henrique <55457217+paulostrix@users.noreply.github.com>
2019-12-15 15:59:20 -0300

    Add files via upload

---
+++ classB.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author selis
+ */
+public class classB {
+    int B1;
+    float B2;
+
+    public int getB1() {
+        return B1;
+    }
+
+    public float getB2() {
+        return B2;
+    }
+
+    public void setB1(int B1) {
+        this.B1 = B1;
+    }
+
+    public void setB2(float B2) {
+        this.B2 = B2;
+    }
+    
+    public void MB1(){
+        System.out.println("MB1");
+    }
+    public void MB2(){
+        System.out.println("MB2");
+    }
+
+}
